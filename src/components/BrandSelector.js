4b27bc308a97533a567fee8eab748580a37371ee
ivstas <stasdat@gmail.com>
2016-05-01 21:16:17 +0300

    all cars filter

--- src/components/BrandSelector.js
+++ src/components/BrandSelector.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default ({brands, onBrandSelect, selectedBrandTitle}) => <ul className="brandSelector">
+    <li onClick={() => onBrandSelect(null)}>all cars</li>
     {brands.map(brand => {
             let brandTitle = brand.get("title");
             let isSelected = brandTitle === selectedBrandTitle;
+
             return <li key={brandTitle}
                        style={isSelected ? {"color": "blue"} : null}
                        onClick={() => onBrandSelect(brandTitle)}>
